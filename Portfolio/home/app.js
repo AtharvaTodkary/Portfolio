10b46ba22f077607da3afdaeb35e41f1e196961e
Atharva Todkary <100767857+AtharvaTodkary@users.noreply.github.com>
2024-01-29 22:24:13 +0530

    Update app.js

--- Portfolio/home/app.js
+++ Portfolio/home/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
-app.use(express.static('public'))
+app.use(express.static('docs'))
 app.use(bodyParser.urlencoded({
     extended:true
 }))
